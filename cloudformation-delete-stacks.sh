799c40d9a16e29dc048c5044c286cae7a132df6d
vkova <vkova@ciklum.com>
2021-09-21 17:33:04 +0300

    fix cloudformation delete file

--- cloudformation-delete-stacks.sh
+++ cloudformation-delete-stacks.sh
@@ -19,7 +19,5 @@ echo -e "${GREEN}Remove vpc stack${RESET}"
 aws cloudformation delete-stack --stack-name vpc
 aws cloudformation wait stack-delete-complete --stack-name vpc
 
-echo -e "${GREEN}Remove github-repos-api-repo stack${RESET}"
+echo -e "${GREEN}Remove image stack${RESET}"
 aws ecr batch-delete-image --repository-name github-repos-api-demo --image-ids imageTag=latest
-aws cloudformation delete-stack --stack-name github-repos-api-repo
-aws cloudformation wait stack-delete-complete --stack-name github-repos-api-repo
